8ddc498cd5a25ce8006285556e4f6a14616b1c31
prem <prem.sherpa@codeimmersives.com>
2020-06-17 14:05:33 -0400

    f1st front end

--- main.js
+++ main.js
@@ -1,5 +1,24 @@
 // Your code here!
+const combinator = require('./color-Combinator');
+const deconstructor = require('./color-Deconstructor.js');
 
+const color1 = getInput(0);
+const color2 = getInput(1);
+
+const combine = combinator(color1, color2);
+const decom = deconstructor(color1);
+
+if(color1 === undefined){
+  console.log('input a color');
+}
+
+if(color1 !== undefined && color2 !== undefined){
+   console.log(combine);
+}
+
+if(color1 !== undefined && color2 === undefined){
+   console.log(decom);
+}
 
 
 // Our helper function here! Don't  touch!
